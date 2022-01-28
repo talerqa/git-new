9a73a7ac76255b69d2409918c72e822a003a2ea4
Aleksei Tarelko <hitri3003@gmail.com>
2022-01-28 18:26:43 +0300

    first com

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+
+const menuList = document.querySelector('.header')
+const activeMenu = document.querySelector('.menu__active');
+menuList.addEventListener('click', event => {
+   if (event.target.closest('.menu__link')) {
+      activeMenu.classList.add('menu__active--active')
+      document.querySelector('.main').classList.add('dark__theme')
+      document.querySelector('.footer').classList.add('dark__theme')
+   } 
+
+   if (!event.target.closest('.menu__link') && !event.target.closest('.menu__active')) {
+    activeMenu.classList.remove('menu__active--active');
+    document.querySelector('.main').classList.remove('dark__theme')
+    document.querySelector('.footer').classList.remove('dark__theme')
+   }
+})
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'Escape') {
+      activeMenu.classList.remove('menu__active--active');
+    }
+})
+   
+const $money = document.querySelector('.header__top-money')
+const $activeMoney = document.querySelector('.header__money');
+
+  $money.addEventListener('click', event =>{
+    if (event.target.closest('.header__top-money')) {
+      
+      $activeMoney.classList.toggle('header__top-money__inner--active');
+    }
+   
+    // if (!event.target.closest('.header__top-money') && !event.target.closest('.header__top-money__inner--active')) {
+    //   $activeMoney.classList.remove('header__top-money__inner--active');
+    // }
+
+    // if(event.target )
+   
+  })
+
+
+
+
+
